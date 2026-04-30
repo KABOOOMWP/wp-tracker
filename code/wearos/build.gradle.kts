4bb49ce2d1fc0e26b921e997f01e980ab47a9ab6
Yan Poblete <yanpob@gmail.com>
2026-04-30 15:20:01 +0200

    Update Gradle version

--- code/wearos/build.gradle.kts
+++ code/wearos/build.gradle.kts
@@ -36,7 +36,10 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig   = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
